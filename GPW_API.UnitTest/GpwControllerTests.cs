e0752b30fb2fd8f30836b6874b0751e8d14ae368
Mateusz Tyczka <mateusz.ty@gmail.com>
2019-11-16 13:51:03 +0100

    Added Unit Tests Prject to the solution

---
+++ GPW_API.UnitTest/GpwControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace GPW_API.UnitTest
+{
+    public class GpwControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
