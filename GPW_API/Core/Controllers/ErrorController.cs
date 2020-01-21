b7b50c614aefa300da76582f2df29c04d8350e7e
Mateusz Tyczka <mateusz.ty@gmail.com>
2020-01-21 13:12:50 +0100

    Added logging

---
+++ GPW_API/Core/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GPW_API.Core.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+
+        private readonly ILogger<ErrorController> _logger;
+
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var exceptionDetail = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            _logger.LogError($"The Path{exceptionDetail.Path} threw an exception: {exceptionDetail.Error}");
+
+
+            return Problem();
+        }
+    }
+}
--- GPW_API/Startup.cs
+++ GPW_API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace GPW_API
 {
@@ -24,11 +25,11 @@ namespace GPW_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-
             var connection = Configuration.GetConnectionString("LocalConnection");
             services.AddDbContext<GpwContext>(opt =>
                 opt.UseSqlServer(connection));
+
+            services.AddLogging();
 
             services.AddScoped<IGpwRepository, GpwRepository>();
 
@@ -38,11 +39,17 @@ namespace GPW_API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile("gpw_logger-{Date}.txt");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
             }
 
             app.UseHttpsRedirection();
